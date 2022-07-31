c25b259d712806e4aad3422f1e2f5ef0f5e2631d
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-07-31 22:52:42 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-async function resolveSample() {
-  return 'resolve!!';
+import logo from "./logo.svg";
+import "./App.css";
+function sampleResolve(value, time) {
+  return new Promise(resolve => {
+      setTimeout(() => {
+          resolve(value);
+          console.log(value);
+      }, time);
+  })
 }
-console.log('01');
-resolveSample().then(value => {
-  console.log(value); // => resolve!!
+
+async function sample() {
+  return await sampleResolve(5, 500) * await sampleResolve(10, 1000) + await sampleResolve(20, 100);
+}
+
+sample().then((v) => {
+  console.log(v);
 });
-console.log('02');
 
 function aFunc3(data) {
-  return new Promise(function(okCallback, ngCallback) {
-      setTimeout(function() {
-          if (Math.random() < 0.30) {
-              ngCallback(new Error('ERROR!'));
-          } else {
-              okCallback(data * 2);
-              setTimeout(() => {
-                okCallback('after setTimeout');
-              },400);
-          }
-      }, Math.random() * 1000);
-  });
+	return new Promise(function (okCallback, ngCallback) {
+		setTimeout(function () {
+			if (Math.random() < 0.3) {
+				ngCallback(new Error("ERROR!"));
+			} else {
+				okCallback(data * 2);
+				setTimeout(() => {
+					okCallback("after setTimeout");
+				}, 400);
+			}
+		}, Math.random() * 1000);
+	});
 }
 
 function sample_reject() {
-  aFunc3(100).then(
-      (data) => { console.log(data); },    // 成功時の処理
-      (e) => { console.log(e); }           // 失敗時の処理
-  );
+	aFunc3(100).then(
+		(data) => {
+			console.log(data);
+		}, // 成功時の処理
+		(e) => {
+			console.log(e);
+		} // 失敗時の処理
+	);
 }
 
 sample_reject();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
