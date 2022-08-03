9a96e2ef65f5f78439b3119df16447a8cfa1afd4
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-08-03 23:14:18 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import "./App.css";
 
-function loadScript(src) {
-	return new Promise((resolve, reject) => {
-		let script = document.createElement("script");
-		script.src = src;
-
-		script.onload = () => resolve(script);
-		script.onerror = () => reject(new Error("Script load error: " + src));
-
-		document.head.append(script);
+function asyncTest() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve('setTimeout end');
+		}, 200);
 	});
 }
 
-let promise = loadScript(
-	"https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js"
-);
+const promiseObj = asyncTest();
 
-promise.then(
-	(script) => alert(`${script.src} is loaded!`),
-	(error) => alert(`Error: ${error.message}`)
-);
-
-promise.then((script) => alert("Another handler..."));
+promiseObj.then((result) => {
+	console.log(result);
+}).catch((error) => {
+	console.log(error);
+}).finally(() => {
+	console.log('finally');
+});
 
 function App() {
 	return <div className="App"></div>;
