bd6c8bbfa922b84b1cacf2c5a4e11f8b6995cbd7
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-08-06 09:28:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
 
 
-fetch('https://httpbin.org/get')
-  .then(response => response.json())
-  .then(data => console.log(data));
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+});
+
+function inc(value) {
+  return value++;
+}
+
+function double(value) {
+  return value*2;
+}
+
+function output(value) {
+  console.log(value);
+}
+
+promise
+.then(inc)
+.then(double)
+.then(output);
 
 function App() {
 	return <div className="App"></div>;
