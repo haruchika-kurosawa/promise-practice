b061b9394699ba1c70d18dad3da03c0d973e2079
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-08-19 22:20:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-function timerPromise(time) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(time);
-    }, time);
-  });
+function noGoodOnReady(fn) {
+	const readyState = document.readyState;
+	// interactive
+	// 文書の読み込みが完了
+	// complete
+	// 文書とすべてのサブリソースの読み込みが完了しました。
+	// この状態は load イベントが発行されようとしていることを示しています。
+	if (readyState === "interactive" || readyState === "complete") {
+		fn();
+	} else {
+		window.addEventListener("DOMContentLoaded", fn);
+	}
 }
 
-Promise.all([
-  timerPromise(100),
-  timerPromise(500),
-  timerPromise(200),
-  timerPromise(300),
-]).then((values) => {
-  console.log(values);
+onReady(() => {
+	console.log("DOM fully loaded and parsed");
+});
+
+
+function goodReadyPromise() {
+  return new Promise((resolve) => {
+      const readyState = document.readyState;
+      if (readyState === "interactive" || readyState === "complete") {
+          resolve();
+      } else {
+          window.addEventListener("DOMContentLoaded", resolve);
+      }
+  });
+}
+goodReadyPromise().then(() => {
+  console.log("DOM fully loaded and parsed");
 });
 
 export default App;
